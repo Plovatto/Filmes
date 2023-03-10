dd7c751f851b599556a373ff192476a37947f2e2
Plovatto <Pandora-blovatto@educar.rs.gov.br>
2023-03-10 15:04:58 -0300

    parte2.0

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ search.addEventListener('input', function() {
     carrosel.style.display = 'none';
   }
 });
+
+let input = document.querySelector("#search");
+let button = document.querySelector("#buttonsearch");
+
+
+
+button.addEventListener("click", function funcao(){
+    if(input.value.length>0){
+let filmes = new Array();
+        fetch("http://www.omdbapi.com/?i=tt3896198&apikey=9d6f18b9="+input.value)
+        .then((resp)=>resp.json())
+        .then((resp)=>{
+resp.Search.forEach((item)=>{
+    let filmes = new Filme
+    
+});
+        });
+
+    }
+return false;
+});
