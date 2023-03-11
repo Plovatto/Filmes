73c079362dfeaf896a6f01f8cc5730b585813076
pandora <pandora-blovatto@educar.rs.gov.br>
2023-03-11 00:11:04 -0300

    Parte2.1

--- js/script.js
+++ js/script.js
@@ -1,32 +1,58 @@
-let search = document.querySelector("#search")
-let carrosel = document.querySelector("#carouselExample")
-
+let search = document.querySelector("#search");
+let carrosel = document.querySelector("#carouselExample");
+let some = document.querySelector("#cardsumir");
 
 search.addEventListener('input', function() {
   if (search.value === '') {
     carrosel.style.display = 'block';
+    some.style.dysplay = 'block';
   } else {
     carrosel.style.display = 'none';
+    some.style.display = 'none';
   }
 });
 
 let input = document.querySelector("#search");
 let button = document.querySelector("#buttonsearch");
 
+button.onclick = async ()=>{
+  if(input.value.length>0){
+    let filmes = new Array();
+    fetch("http://www.omdbapi.com/?apikey=9d6f18b9&s=" + input.value,{mode:"cors"})
+      .then((resp)=>resp.json())
+      .then((resp)=>{
+        resp.Search.forEach((item)=>{
+          let filme = new Filme(
+            item.imdbID,
+            item.Title,
+            item.Year,
+            item.Genre,
+            null,
+            item.Poster,
+            null,
+            null,
+            null,
+            null,
+            null
+          );
+          filmes.push(filme);
+        }); 
+        listarFilmes(filmes);
+        
+      });
+  }
+  return false;
+};
 
 
-button.addEventListener("click", function funcao(){
-    if(input.value.length>0){
-let filmes = new Array();
-        fetch("http://www.omdbapi.com/?i=tt3896198&apikey=9d6f18b9="+input.value)
-        .then((resp)=>resp.json())
-        .then((resp)=>{
-resp.Search.forEach((item)=>{
-    let filmes = new Filme
-    
-});
-        });
+let listarFilmes = async (filmes) => {
+  let listaFilmes = await document.querySelector("#listaFilmes");
+  listaFilmes.innerHTML = "";
+  if(filmes.length > 0) {
+    filmes.forEach(async (filme) => {
+      listaFilmes.appendChild(await filme.getCard());
+    });
+  }
+  return false;
+};
 
-    }
-return false;
-});
--- js/script2.js
+++ js/script2.js
@@ -1,77 +1,79 @@
-
-class Ator{
-    constructor(id, nome){
-        this.nome = nome;
-        this.id = id;
+ 
+  class Ator {
+    constructor(id, nome) {
+      this.nome = nome;
+      this.id = id;
     }
-}
-
-class Diretor{
-    constructor(nome, id){
-        this.nome = nome;
-        this.id = id;
+  }
+  
+  class Diretor {
+    constructor(nome, id) {
+      this.nome = nome;
+      this.id = id;
     }
-}
-
-class Filme{
-    constructor(id, titulo, ano, genero, duracao, cartaz, sipnose, direcao, elenco, classificacao, avaliacao){
-
-        this.id=id;
-        this.genero=genero;
-        this.titulo=titulo;
-        this.ano=ano;
-        this.duracao=duracao;
-        this.cartaz=cartaz;
-        this.sipnose=sipnose;
-        this.direcao=direcao;
-        this.elenco=elenco;
-        this.classificacao=classificacao;
-        this.avaliacao=avaliacao;
-
+  }
+  
+  class Filme {
+    constructor(id, titulo, ano, genero, duracao, cartaz, sipnose, direcao, elenco, classificacao, avaliacao) {
+      this.id = id;
+      this.genero = genero;
+      this.titulo = titulo;
+      this.ano = ano;
+      this.duracao = duracao;
+      this.cartaz = cartaz;
+      this.sipnose = sipnose;
+      this.direcao = direcao;
+      this.elenco = elenco;
+      this.classificacao = classificacao;
+      this.avaliacao = avaliacao;
     }
+  
+    getCard = async () => {
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
 
-getCard = async()=>{
-    let card = document.createElement("div");
-    card.setAttribute("class","card");
+   
+      const imgCartaz = document.createElement("img");
+      imgCartaz.setAttribute("class", "card-img-top");
+      imgCartaz.setAttribute("src", this.cartaz);
+  
+      const cardBody = document.createElement("div");
+      cardBody.setAttribute("class", "card-body");
+  
+      const hCardTitle = document.createElement("h5");
+      hCardTitle.setAttribute("class", "card-title");
+  
+      const divDetalhes = document.createElement("div");
+      divDetalhes.setAttribute("style", "display:flex; justify-content:space-around;");
+  
+      const divGenero = document.createElement("div");
+      divGenero.setAttribute("style", "flex-row:1;");
+  
+      const divAnoProducao = document.createElement("div");
+      divAnoProducao.setAttribute("style", "flex-row:1;");
+  
+      const divClassificacao = document.createElement("div");
+      divClassificacao.setAttribute("style", "flex-row:1;");
+  
+      hCardTitle.appendChild(document.createTextNode(this.titulo));
+      divAnoProducao.appendChild(document.createTextNode(this.ano));
+      divGenero.appendChild(document.createTextNode(this.genero));
+      divClassificacao.appendChild(document.createTextNode(this.classificacao));
+  
+      divDetalhes.appendChild(divGenero);
+      divDetalhes.appendChild(divAnoProducao);
+      divDetalhes.appendChild(divClassificacao);
+      card.appendChild(imgCartaz);
+      card.appendChild(cardBody);
+      cardBody.appendChild(hCardTitle);
+      cardBody.appendChild(divDetalhes);
 
-    let imgCartaz = document.createElement("img");
-    imgCartaz.setAttribute("class","card-img-topz");
-    imgCartaz.setAttribute("src",this.cartaz);
+ 
+      return card;
 
-    let cardBody = document.createElement("div");
-    cardBody.setAttribute("class","card-body");
-
-    let hCardTitle = document.createElement("h5");
-    hCardTitle.setAttribute("class","card-title")
-
-    let divDetalhes = document.createElement("div");
-    divDetalhes.setAttribute("style","display:flex; justify-content:space-aroud;");
-
-    divGenero = document.createElement("div");
-    divGenero.setAttribute("style","flex-grow:1;");
-
-    let divAnoProducao = document.createElement("div");
-    divAnoProducao.setAttribute("style","flex-grow:1;");
-
-    let divClassificacao = document.createElement("div");
-    divClassificacao.setAttribute("style","flex-grow:1;");
-
-    hCardTitle.appendChild(document.createTextNode(this.titulo));
-    divAnoProducao.appendChild(document.createTextNode(this.ano));
-    divGenero.appendChild(document.createTextNode(this.genero));
-    divClassificacao.appendChild(document.createTextNode(this.classificacao));
-
-    divDetalhes.appendChild(divGenero);
-    divDetalhes.appendChild(divAnoProducao);
-    divDetalhes.appendChild(divClassificacao);
-    card.appendChild(imgCartaz);
-    card.appendChild(cardBody);
-    cardBody.appendChild(hCardTitle);
-    cardBody.appendChild(divDetalhes);
-    return card;
-    
-}
+      
+    };
+  }
 
 
-}
-
+ 
